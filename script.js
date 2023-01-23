eb4ed13aa89fc20b28497ea2011b1fa253e583da
Adrian Mora <105446015+moraadrian510@users.noreply.github.com>
2023-01-23 11:10:49 -0800

    js appi searc code inserted

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+var apiKey = '624172607408572c325d20c8a89e7175'
+
+
+
 // gathers weather info for weather widgets.
 !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src='https://weatherwidget.io/js/widget.min.js';fjs.parentNode.insertBefore(js,fjs);}}(document,'script','weatherwidget-io-js');
 
@@ -10,3 +14,33 @@ var today = dayjs();
  $('#current-weather-header').text(today.format('MMM D, YYYY hh:mm:ss a'));
 }
 
+function getCurrentWeather(city) {
+    var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`
+    fetch(url)
+    .then((response)=>{
+        return response.json();
+    })
+    .then((data)=>{
+        console.log(data);
+        var lon = data.coord.lon 
+        var lat = data.coord.lat
+        getOneCall(lat, lon)
+    })
+}
+
+function getOneCall(lat, lon) {
+    var url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude={minutely,hourly,alerts}&appid=${apiKey}`
+    fetch(url)
+    .then((response)=>{
+        return response.json();
+    })
+    .then((data)=>{
+        console.log(data);
+    })
+}
+
+$('#search-btn').click(function(){
+    var searchInput = $('#input-search').val()
+    console.log(searchInput);
+    getCurrentWeather(searchInput)
+})
